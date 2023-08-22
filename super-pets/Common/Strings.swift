0dafe5ec0499ef29dd20350e4545785d4675f9bd
babarone22 <barbara_barone2@hotmail.com>
2023-08-21 22:42:53 -0300

    adicionando cores, imagem e implementando primeiros componentes da home

---
+++ super-pets/Common/Strings.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+enum Strings {
+    enum Color {
+        static let background = "clr_background"
+        static let darkGray = "clr_dark_gray"
+        static let lightGray = "clr_light_gray"
+        static let lightOrange = "clr_light_orange"
+        static let lightPurple = "clr_light_purple"
+        static let lightRouse = "clr_light_rouse"
+        static let orange = "clr_orange"
+        static let purple = "clr_purple"
+        static let rouse = "clr_rouse"
+        static let white = "clr_white"
+    }
+    
+    enum Images {
+        enum Ilustrations {
+            static let familyPets = "ilu_family_pets"
+        }
+    }
+}
--- super-pets/Scenes/Home/HomeViewController.swift
+++ super-pets/Scenes/Home/HomeViewController.swift
@@ -1,6 +1,29 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
+    
+    private lazy var appLabel: UILabel = {
+        let label = UILabel()
+        label.text = "SuperPets"
+        label.textColor = UIColor(named: Strings.Color.rouse)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Adoção é um ato de  amor e responsabilidade"
+        label.textColor = UIColor(named: Strings.Color.lightGray)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var cellImage: UIImageView = {
+        let image = UIImageView()
+        image.image = .init(named: Strings.Images.Ilustrations.familyPets)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
