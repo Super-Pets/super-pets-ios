12e7470ae4c8ecc9083f9366a2bddd64d22cb547
babarone22 <barbara_barone2@hotmail.com>
2023-08-25 16:12:28 -0300

    iniciando implementação camada de network

--- super-pets/Scenes/Network/ApiService.swift
+++ super-pets/Scenes/Network/ApiService.swift
@@ -1,5 +1,43 @@
 import Foundation
 
-final class ApiService {
+enum ApiError: Error {
+    case invalidURL,
+         requestError(description: String),
+         invalidResponse,
+         invalidData,
+         decodingError(description: String)
+}
+
+protocol ApiServiceProtocol {
     
+}
+
+final class ApiService: ApiServiceProtocol {
+    private func fetchData<T: Codable>(url: URL?, completion: @escaping (Result<T, ApiError>) -> Void) {
+        guard let url = url else {
+            return completion(.failure(ApiError.invalidURL))
+        }
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(.requestError(description: error.localizedDescription)))
+                return
+            }
+            
+            guard let data = data else {
+                return completion(.failure(.invalidData))
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                let result = try decoder.decode(T.self, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(.decodingError(description: error.localizedDescription)))
+            }
+        }
+        dataTask.resume()
+    }
 }
